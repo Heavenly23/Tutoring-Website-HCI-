46378d4fe194c178fb3973224d7d77d582737413
MerryH23 <32797978+Heavenly23@users.noreply.github.com>
2020-05-19 05:23:01 +0900

    onclick events added

--- User Research/Initial Prototype/index.js
+++ User Research/Initial Prototype/index.js
@@ -110,3 +110,15 @@ function checkInput_6(){
         document.getElementById("7").style.display = "block";
   }, 3000);
 }
+
+function display(id){
+  document.getElementById("1").style.display = "none";
+  document.getElementById("2").style.display = "none";
+  document.getElementById("3").style.display = "none";
+  document.getElementById("4").style.display = "none";
+  document.getElementById("5").style.display = "none";
+  document.getElementById("6").style.display = "none";
+  document.getElementById(id).style.display = "block";;
+
+
+}
